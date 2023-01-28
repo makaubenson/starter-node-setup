c7ed148db65ba6ed3a4c1836836e5e4f8be7b997
aknjoroge <alexkaranja629@gmail.com>
2023-01-28 09:10:28 +0300

    addded error code in paths

--- controller/mainController.js
+++ controller/mainController.js
@@ -16,3 +16,17 @@ exports.getHome = AsyncError(async function (req, res) {
       allLocation: "70",
     });
 });
+
+exports.getNotFound = AsyncError(async function (req, res) {
+  let code = req.path.includes("404") ? "404" : "500";
+
+  res
+    .status(200)
+    .set(
+      "Content-Security-Policy",
+      "default-src 'self' https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js  ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render("_404", {
+      code,
+    });
+});
--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -7,6 +7,8 @@ let router = express.Router();
 // router.use(authController.isLoggedin);
 //pug rendering
 router.get("/", authController.isLoggedin, mainController.getHome);
+router.get("/404", authController.isLoggedin, mainController.getNotFound);
+router.get("/500", authController.isLoggedin, mainController.getNotFound);
 router.post("/forgotPassword/", authController.forgotPassword);
 
 module.exports = router;
